061b6dee3f5dd8878d4257b1a791aad53ca70a82
jayokeyo <jaykopiyo@gmail.com>
2023-11-03 18:08:12 +0000

    Rolled back to wildcard address and inclded POST method

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -3,16 +3,19 @@
 Create a single / route and an index.html template that simply outputs 
 “Welcome to Holberton” as page title (<title>) and “Hello world” as header (<h1>).
 '''
+
+
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
-@app.route('/', methods = ['GET'], strict_slashes=False)
+@app.route('/', methods = ['GET', 'POST'], strict_slashes=False)
 def index():
-    '''Render index.html'''
+    '''Render index.html
+    '''
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port='5000')
+    app.run(host='0.0.0.0', port='5000')
